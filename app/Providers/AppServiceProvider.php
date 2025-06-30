bfd9e08d5f86646e9d5912737a0e2f4730952f79
ht3aa <ht3aa2001@gmail.com>
2025-06-30 12:51:36 +0300

    adding global configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+        ViewAction::configureUsing(function (ViewAction $action) {
+            $action
+                ->icon('heroicon-o-eye')
+                ->iconButton();
+        });
     }
 }
